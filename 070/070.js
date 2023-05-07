7fe79d8d7986c74b4ebd48ab4163e83fe618d375
yaojinxi <864554492@qq.com>
2023-05-07 23:46:05 +0800

    creat 070

---
+++ 070/070.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: yaojinxi 864554492@qq.com
+ * @Date: 2023-05-07 23:45:26
+ * @LastEditors: yaojinxi 864554492@qq.com
+ * @LastEditTime: 2023-05-07 23:45:48
+ * @FilePath: \leetcode\070\070.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+let test = '123'
