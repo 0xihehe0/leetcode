27350c3df6b332bf4b5879327baf6297b2e6a1c7
yaojinxi <864554492@qq.com>
2023-05-08 23:26:07 +0800

    creat fucntion

--- 070/070.js
+++ 070/070.js
@@ -2,8 +2,11 @@
  * @Author: yaojinxi 864554492@qq.com
  * @Date: 2023-05-07 23:45:26
  * @LastEditors: yaojinxi 864554492@qq.com
- * @LastEditTime: 2023-05-07 23:45:48
+ * @LastEditTime: 2023-05-08 23:25:56
  * @FilePath: \leetcode\070\070.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 let test = '123'
+function step(){
+    return '123'
+}
