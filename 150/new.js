5094fa5b8780296c3992d22901a7e7eee57d45e1
yaojinxi <864554492@qq.com>
2023-05-13 23:41:14 +0800

    change

--- 150/new.js
+++ 150/new.js
@@ -2,8 +2,8 @@
  * @Author: yaojinxi 864554492@qq.com
  * @Date: 2023-05-12 23:39:40
  * @LastEditors: yaojinxi 864554492@qq.com
- * @LastEditTime: 2023-05-12 23:39:52
+ * @LastEditTime: 2023-05-13 23:41:03
  * @FilePath: \leetcode\150\new.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-let ee = '123'
+let ee = '1234'
