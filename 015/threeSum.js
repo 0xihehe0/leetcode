3c84436342e19c42f1f468ff4b164cfcc2f62b80
姚进玺 <yaojinxi@bovosz.com>
2023-06-07 17:16:47 +0800

    creat 015

---
+++ 015/threeSum.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: 姚进玺 yaojinxi@bovosz.com
+ * @Date: 2023-06-05 16:04:12
+ * @LastEditors: 姚进玺 yaojinxi@bovosz.com
+ * @LastEditTime: 2023-06-05 17:33:34
+ * @FilePath: /leetcode/leetcode/015/threeSum.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    for (var i = 0; i < nums.length; i++){
+      console.log(nums[i]);
+    }
+    return arr
+};
+
+let nums = [-1, 0, 1, 2, -1, -4];
+console.log(threeSum(nums));
