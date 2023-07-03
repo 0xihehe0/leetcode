6d00937cd9c598d8e12f396ebadbeedf448a6196
yaojinxi <864554492@qq.com>
2023-07-03 22:01:40 +0800

    change 088

--- 088/MergeSortedArray.js
+++ 088/MergeSortedArray.js
@@ -2,7 +2,7 @@
  * @Author: yaojinxi 864554492@qq.com
  * @Date: 2023-05-11 12:06:59
  * @LastEditors: yaojinxi 864554492@qq.com
- * @LastEditTime: 2023-05-29 23:37:34
+ * @LastEditTime: 2023-07-03 22:01:28
  * @FilePath: \leetcode\088\MergeSortedArray.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,8 +11,6 @@ var merge = function (nums1, m, nums2, n) {
         p2 = n - 1;
     let tail = m + n - 1;
     var cur;
-    console.log(p1, p2);
-    console.log(tail);
     while (p1 >= 0 || p2 >= 0) {
         console.log(p1, p2);
         if (p1 === -1) {
