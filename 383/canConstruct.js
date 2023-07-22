2a4e034edeac6348faffbe7fa8a75b5c01a56720
yaojinxi <864554492@qq.com>
2023-07-22 18:39:19 +0800

    change383

--- 383/canConstruct.js
+++ 383/canConstruct.js
@@ -2,7 +2,7 @@
  * @Author: yaojinxi 864554492@qq.com
  * @Date: 2023-06-25 16:35:23
  * @LastEditors: yaojinxi 864554492@qq.com
- * @LastEditTime: 2023-07-21 20:06:22
+ * @LastEditTime: 2023-07-21 23:20:00
  * @FilePath: \leetcode\leetcode\383\canConstruct.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -25,6 +25,6 @@ var canConstruct = function (ransomNote, magazine) {
     return true
 };
 
-let ransomNote = 'azazaz',
+let ransomNote = 'azazazqwe',
     magazine = 'zbaa';
 console.log(canConstruct(ransomNote, magazine));
