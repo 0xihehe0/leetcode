745e46272b2d4fa1f82f20b880fe0a3b9ed2adf2
yaojinxi <864554492@qq.com>
2024-03-22 00:13:27 +0000

    finish 154

--- 154/findMin.js
+++ 154/findMin.js
@@ -2,7 +2,7 @@
  * @Author: yaojinxi 864554492@qq.com
  * @Date: 2024-03-21 23:39:49
  * @LastEditors: yaojinxi 864554492@qq.com
- * @LastEditTime: 2024-03-22 00:12:53
+ * @LastEditTime: 2024-03-22 00:12:36
  * @FilePath: \leetcode\153\findMin.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,17 +13,23 @@
 var findMin = function (nums) {
     let left = 0;
     let right = nums.length - 1;
+    if (nums[left] < nums[right]) {
+        return nums[left];
+    }
 
     while (left < right) {
         let mid = left + Math.floor((right - left) / 2);
+        console.log(mid, right, left);
         if (nums[mid] < nums[right]) {
             right = mid;
-        } else {
+        } else if (nums[mid] > nums[right]) {
             left = mid + 1;
+        } else {
+            right--;
         }
     }
     return nums[left];
 };
 
-let nums = [3, 4, 1, 2];
+let nums = [3, 3, 1, 3];
 console.log(findMin(nums));
