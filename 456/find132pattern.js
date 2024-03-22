08e736486f8f528adcf01a6b671b799b78d801c9
yaojinxi <864554492@qq.com>
2024-03-22 18:02:14 +0000

    wrong 456

--- 456/find132pattern.js
+++ 456/find132pattern.js
@@ -2,7 +2,17 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var find132pattern = function (nums) {};
+var find132pattern = function (nums) {
+    let index = 0;
+    let i = nums[0];
+    while (index < nums.length) {
+        
+        index++;
+        console.log(index);
+    }
 
-let nums = [3, 1, 4, 2];
+    return false;
+};
+
+let nums = [3, 0, 1, 2];
 console.log(find132pattern(nums));
