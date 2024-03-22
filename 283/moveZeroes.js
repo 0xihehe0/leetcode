9284b0ce746d89032e0237169de05b8009cd4f38
yaojinxi <864554492@qq.com>
2024-03-22 18:49:35 +0000

    creat 283

---
+++ 283/moveZeroes.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: yaojinxi 864554492@qq.com
+ * @Date: 2024-03-22 18:49:22
+ * @LastEditors: yaojinxi 864554492@qq.com
+ * @LastEditTime: 2024-03-22 18:49:29
+ * @FilePath: \leetcode\283\moveZeroes.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+
+};
