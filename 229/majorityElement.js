8108166d9d28f1fcc394e09bb96bfd167d26c6fd
yaojinxi <864554492@qq.com>
2024-03-22 22:34:27 +0000

    creat 229

---
+++ 229/majorityElement.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: yaojinxi 864554492@qq.com
+ * @Date: 2024-03-22 22:33:52
+ * @LastEditors: yaojinxi 864554492@qq.com
+ * @LastEditTime: 2024-03-22 22:34:09
+ * @FilePath: \leetcode\229\majorityElement.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var majorityElement = function(nums) {
+
+};
+
+let nums = [1,1,2,2,3,3,4,,4,4,4,4,4]
+console.log(majorityElement(nums));
