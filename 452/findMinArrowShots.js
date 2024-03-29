5e9039eb9adf04248f10f9d1cd2d66af0f3a6923
yaojinxi <864554492@qq.com>
2024-03-29 13:26:34 +0000

    creat 452

---
+++ 452/findMinArrowShots.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: yaojinxi 864554492@qq.com
+ * @Date: 2024-03-29 13:01:54
+ * @LastEditors: yaojinxi 864554492@qq.com
+ * @LastEditTime: 2024-03-29 13:02:22
+ * @FilePath: \leetcode\452\findMinArrowShots.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function (points) {
+    
+};
+
+let points = [
+    [10, 16],
+    [2, 8],
+    [1, 6],
+    [7, 12]
+];
+console.log(findMinArrowShots(points));
