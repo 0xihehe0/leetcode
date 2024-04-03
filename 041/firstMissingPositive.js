43fb5be70cb74c1217bc20adbdfbb1ec5cd21ea7
yaojinxi <864554492@qq.com>
2024-04-03 23:42:14 +0100

    up data

--- 041/firstMissingPositive.js
+++ 041/firstMissingPositive.js
@@ -2,7 +2,7 @@
  * @Author: yaojinxi 864554492@qq.com
  * @Date: 2024-03-30 15:06:46
  * @LastEditors: yaojinxi 864554492@qq.com
- * @LastEditTime: 2024-03-30 15:07:45
+ * @LastEditTime: 2024-04-03 23:41:39
  * @FilePath: \leetcode\041\firstMissingPositive.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,3 +13,5 @@
 var firstMissingPositive = function(nums) {
 
 };
+
+let nums = [1,2,3,4]
