9cebdba7e3536217376e8ba759ab3aa52eeff6ca
yaojinxi <864554492@qq.com>
2024-04-06 11:39:43 +0100

    creat 390

---
+++ 390/lastRemaining.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: yaojinxi 864554492@qq.com
+ * @Date: 2024-04-06 11:37:37
+ * @LastEditors: yaojinxi 864554492@qq.com
+ * @LastEditTime: 2024-04-06 11:37:56
+ * @FilePath: \leetcode\390\lastRemaining.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+var lastRemaining = function (n) {};
+
+let n = [1, 2, 3, 4, 5, 6, 7, 8];
