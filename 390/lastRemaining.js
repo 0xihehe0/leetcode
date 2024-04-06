e2bdd248cf13a358887304eae1f771cd488ba07c
yaojinxi <864554492@qq.com>
2024-04-06 14:15:42 +0100

    while 390 change

--- 390/lastRemaining.js
+++ 390/lastRemaining.js
@@ -2,40 +2,27 @@
  * @Author: yaojinxi 864554492@qq.com
  * @Date: 2024-04-06 11:37:37
  * @LastEditors: yaojinxi 864554492@qq.com
- * @LastEditTime: 2024-04-06 13:51:24
+ * @LastEditTime: 2024-04-06 14:03:21
  * @FilePath: \leetcode\390\lastRemaining.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 var lastRemaining = function (n) {
-    let result = [];
-    for (let i = 1; i <= n; i++) {
-        result.push(i);
-    }
-    return checkHalf(result, true);
-};
+    let remain = n;
+    let step = 1;
+    let head = 1;
+    let left = true;
 
-function checkHalf(n, type) {
-    if (n.length === 1) {
-        return n[0];
-    }
-    console.log(n);
-    let nextArray = [];
-    if (type) {
-        for (let i = 0; i < n.length; i++) {
-            if (i % 2 !== 0) {
-                nextArray.push(n[i]);
-            }
+    while (remain > 1) {
+        // 如果是从左到右，或者从右到左但剩余数量是奇数
+        if (left || remain % 2 === 1) {
+            head += step;
         }
-    } else {
-        n = n.reverse();
-        for (let i = 0; i < n.length; i++) {
-            if (i % 2 !== 0) {
-                nextArray.unshift(n[i]);
-            }
-        }
+        remain = Math.floor(remain / 2);
+        step *= 2;
+        left = !left;
     }
-    return checkHalf(nextArray, !type);
-}
+    return head;
+};
 
 let n = 6;
 console.log(lastRemaining(n));
