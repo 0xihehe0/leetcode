f50cf87342b5fdf9e5521d12c0959e7931095fd7
yaojinxi <864554492@qq.com>
2024-04-06 14:41:17 +0100

    change 144

--- 144/preorderTraversal.js
+++ 144/preorderTraversal.js
@@ -2,7 +2,7 @@
  * @Author: yaojinxi 864554492@qq.com
  * @Date: 2024-04-06 14:17:44
  * @LastEditors: yaojinxi 864554492@qq.com
- * @LastEditTime: 2024-04-06 14:28:17
+ * @LastEditTime: 2024-04-06 14:36:43
  * @FilePath: \leetcode\144\preorderTraversal.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -41,7 +41,6 @@ var preorderTraversal = function (root) {
         dfs(root.right);
     }
     dfs(root);
-
     return list
 };
 
