9a720c83d8e01569de9ae0fa9f7c818bf186859c
yaojinxi <864554492@qq.com>
2024-04-08 12:15:11 +0100

    creat 637

---
+++ 637/averageOfLevels.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: yaojinxi 864554492@qq.com
+ * @Date: 2024-04-08 12:13:35
+ * @LastEditors: yaojinxi 864554492@qq.com
+ * @LastEditTime: 2024-04-08 12:13:43
+ * @FilePath: \leetcode\637\averageOfLevels.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+var averageOfLevels = function(root) {
+
+};
+
