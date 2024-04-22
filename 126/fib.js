c61a86705b83696042aa252de946a183aac3a95c
yaojinxi <864554492@qq.com>
2024-04-22 12:11:49 +0100

    creat 126

---
+++ 126/fib.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: yaojinxi 864554492@qq.com
+ * @Date: 2024-04-22 12:10:46
+ * @LastEditors: yaojinxi 864554492@qq.com
+ * @LastEditTime: 2024-04-22 12:11:41
+ * @FilePath: \leetcode\126\fib.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+var fib = function(n) {
+
+};
+
+let n = 4
+console.log(fib(n));
